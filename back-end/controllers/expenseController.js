d239b0edacf89cb4b090bdcddf46988c844d20b6
KavithRashintha <wgkrashintha@gmail.com>
2024-09-26 15:05:44 +0530

    Update and Get All Functions Created

--- back-end/controllers/expenseController.js
+++ back-end/controllers/expenseController.js
@@ -19,6 +19,43 @@ const addExpense = async (req,res) => {
     }
 }
 
+const updateExpense = async (req,res) => {
+    const { id } = req.params;
+    const { title, description, date, category, amount } = req.body;
+
+    try{
+        const updatedExpense = await ExpenseModel.findByIdAndUpdate(
+            id,
+            { title, description, date, category, amount }
+        );
+
+        if(!updatedExpense) {
+            return res.status(404).json({ message: 'Expense not found' });
+        }
+
+        res.status(200).json('Successfully Updated');
+    }catch (error){
+        res.status(422).json({ error: 'Failed to update the expense', details: error });
+    }
+}
+
+const getAllExpenses = async (req,res) => {
+
+    try{
+        const expenses = await ExpenseModel.find();
+
+        if(!expenses){
+            return res.status(404).json({ message: 'Expenses not found' });
+        }
+
+        res.status(200).json(expenses);
+    }catch (error){
+        res.status(500).json({ error: 'Failed to fetch expenses', details: error });
+    }
+}
+
 module.exports = {
-    addExpense
+    addExpense,
+    updateExpense,
+    getAllExpenses
 }
--- back-end/routes/expenseRoute.js
+++ back-end/routes/expenseRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {addExpense} = require('../controllers/expenseController');
+const {addExpense, updateExpense, getAllExpenses} = require('../controllers/expenseController');
 
 const route = express.Router();
 
 route.post('/addExpense', addExpense);
+route.put('/updateExpense/:id', updateExpense);
+route.get('/getAllExpenses', getAllExpenses);
 
 module.exports = route;
